823b18d3e8bedb1cdeeb75be32204c97c65609bd
Mauricio Valdez <85323904+209mv@users.noreply.github.com>
2026-04-05 10:21:19 -0700

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,12 @@
+// Navigate to game page
+function openGame(gameUrl) {
+  window.location.href = `gamepage.html?game=${gameUrl}`;
+}
+
+// Placeholder login/register
+function login() {
+  alert('Login not implemented yet!');
+}
+function register() {
+  alert('Register not implemented yet!');
+}
